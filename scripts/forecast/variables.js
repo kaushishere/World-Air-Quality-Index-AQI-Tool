80fb030e3e16de267fd34064dbfd8af295a4d9ef
kaushishere <kaushal.modhwadia@gmail.com>
2025-11-06 18:16:31 +0000

    update to DeepAR_3W model attributes

--- scripts/forecast/variables.js
+++ scripts/forecast/variables.js
@@ -15,7 +15,7 @@ const models = [{
 {
     name: 'DeepAR_3W',
     description: "DeepAR is an AWS built-in algorithm for predicting timeseries data. The model's hyperparameters include a context window of 24 hours, and a prediction window of 24 hours also. This model has been deployed to a serverless endpoint and so it can take up to 10 seconds to yield the first set of predicted values.",
-    trainingSize: "Training dataset size: 2 weeks",
+    trainingSize: "Training dataset size: 3 weeks",
     endpointName: "deepar3w-aqi-serverless-endpoint"
 }
 ]
