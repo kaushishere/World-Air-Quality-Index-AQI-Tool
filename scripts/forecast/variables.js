652221c6310c80ad50ef30c12e22819b16b6c66d
kaushishere <kaushal.modhwadia@gmail.com>
2025-11-13 09:33:38 +0000

    Added DeepAR_4W model

--- scripts/forecast/variables.js
+++ scripts/forecast/variables.js
@@ -17,6 +17,12 @@ const models = [{
     description: "DeepAR is an AWS built-in algorithm for predicting timeseries data. The model's hyperparameters include a context window of 24 hours, and a prediction window of 24 hours also. This model has been deployed to a serverless endpoint and so it can take up to 10 seconds to yield the first set of predicted values.",
     trainingSize: "Training dataset size: 3 weeks",
     endpointName: "deepar3w-aqi-serverless-endpoint"
+},
+{
+    name: 'DeepAR_4W',
+    description: "DeepAR is an AWS built-in algorithm for predicting timeseries data. The model's hyperparameters include a context window of 24 hours, and a prediction window of 24 hours also. This model has been deployed to a serverless endpoint and so it can take up to 10 seconds to yield the first set of predicted values.",
+    trainingSize: "Training dataset size: 4 weeks",
+    endpointName: "deepar4w-aqi-serverless-endpoint"
 }
 ]
 
